77c177870c9dcc339f101f075b6dd6552aba6014
Georgina Neilson <Georgina.Neilson@qnrl.com>
2018-04-09 10:50:44 +0100

    Complete pixel grid generating code

--- site/designs.js
+++ site/designs.js
@@ -2,41 +2,64 @@ $(document).ready(function() {
   console.log("document ready");
 
 
-  $("#buttonSubmit").click(function() {
-    console.log("something");
-    $("form").append("<p>added this</p>")
-    return false;
+  $("#buttonSubmit").click(function(e) {
+    e.preventDefault();
+    console.log("button submitted");
+    $( "#pixelCanvas" ).empty();
+    console.log("canvas cleared?")
+    const height = $("#inputHeight").val();
+    const width = $("#inputWidth").val();
+    console.log(height + " " + width);
+
+    var i;
+    if (height < 1) {
+      console.log("less than 1")
+    } else {
+
+      var heightOutput = "";
+      for (i = 0; i < height; i++) {
+        heightOutput += "<tr></tr>\n";
+        console.log(heightOutput);
+
+      }
+
+      if (width < 1) {
+        console.log("width less than 1")
+      } else {
+
+        var widthOutput = "";
+        for (i = 0; i < width; i++) {
+          widthOutput += "<td></td>";
+          console.log(widthOutput);
+
+        }
+      }
+
+
+      $( "#pixelCanvas" ).append(heightOutput);
+      $( "tr" ).append(widthOutput);
+    }
+
   });
 
-
-  });
-
-
-  // Select color input
-  // Select size input
-// $('#buttonSubmit').click(function getUserInput() {
-//
-//   const height = $('#inputHeight').val();
-//   const width = $('#inputWidth').val();
-//
-//       console.log(height);
-//       console.log(width);
-//
-// });
-
-
-
-
-
-
-// $('#buttonSubmit').click(function getUserInput() {
-//
-// console.log("clicked");
-//
-// });
 
   function makeGrid() {
-
-    // Your code goes here!
+    console.log("makegrid")
+    // var i;
+    // if (height < 1) {
+    //   console.log("less than 1")
+    // } else {
+    //   for (i = 0; i < height; i++) {
+    //     let heightOutput;
+    //     heightOutput + ("<tr></tr>" + \n)
+    //     console.log("more than one!")
+    //   }
+    // }
 
   }
+
+});
+
+
+// Select color input
+// Select size input
